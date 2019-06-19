7102a2dcc369e7dbd86bd4ec679ae2d113703dd4
zixiamu <1214145161@qq.com>
2019-06-19 21:23:01 +0800

    更新其他文件

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import  Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component("g-input",Input)
+
 
 var app = new Vue({
     el:'#app',
