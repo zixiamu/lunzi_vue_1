10211ff166dffca33e873c208fd89b29b25d3603
zixiamu <1214145161@qq.com>
2019-06-21 15:13:47 +0800

    基本是实现row 和col的功能

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component("g-input",Input)
+Vue.component("g-row",Row)
+Vue.component("g-col",Col)
 
 
 var app = new Vue({
@@ -25,100 +29,4 @@ var app = new Vue({
     }
 })
 
-import  chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
 
-const  expect =chai.expect
-
-//单元测试
-//测试按钮含有icon
-try{
-    {
-        const Contructor = Vue.extend(Button)
-        const vm = new Contructor({
-            propsData:{
-                icon:'settings'
-            }
-        })
-        vm.$mount()
-        let useElement=vm.$el.querySelector('use')
-        let href=useElement.getAttribute('xlink:href')
-        expect(href).to.eq('#i-settings')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-    {
-        const Contructor = Vue.extend(Button)
-        const vm = new Contructor({
-            propsData:{
-                icon:'settings',
-                loading:true
-            }
-        })
-        vm.$mount()
-        let useElement=vm.$el.querySelector('use')
-        let href=useElement.getAttribute('xlink:href')
-        expect(href).to.eq('#i-loading')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-    {
-        const div = document.createElement('div')
-        document.body.appendChild(div)
-        const Contructor = Vue.extend(Button)
-        const vm = new Contructor({
-            propsData:{
-                icon:'settings',
-                iconPosition:'left'
-            }
-        })
-        vm.$mount(div)
-        let svg=vm.$el.querySelector('svg')
-        let {order}=window.getComputedStyle(svg)
-        expect(order).to.eq('1')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-    {
-        const div = document.createElement('div')
-        document.body.appendChild(div)
-        const Contructor = Vue.extend(Button)
-        const vm = new Contructor({
-            propsData:{
-                icon:'settings',
-                iconPosition:'right'
-            }
-        })
-        vm.$mount(div)
-        let svg=vm.$el.querySelector('svg')
-        let {order}=window.getComputedStyle(svg)
-        expect(order).to.eq('2')
-        vm.$el.remove()
-        vm.$destroy()
-    }
-
-//mock
-    {
-        const Contructor = Vue.extend(Button)
-        const vm = new Contructor({
-            propsData: {
-                icon: 'settings',
-            }
-        })
-        vm.$mount()
-        let spy = chai.spy(function () {
-        })
-        vm.$on('click', spy)
-        //希望函数被执行
-        let button = vm.$el
-        button.click()
-        expect(spy).to.have.been.called()
-    }
-} catch(error){
-    window.errors=[error]
-} finally {
-    window.errors && window.errors.forEach((error)=>{
-        console.error(error.message)
-    })
-}
