a5ef7348176df4aa3c0f9c1cc068a7dc0ebca776
zixiamu <1214145161@qq.com>
2019-06-30 11:19:55 +0800

    collapse基本样式和基本功能完成

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ import TabsBody from './tabs-body'
 import TabsItem from  './tabs-item'
 import TabsPane from './tabs-pane'
 import Popver from './popover'
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
@@ -38,6 +40,8 @@ Vue.component("g-tabs-item",TabsItem)
 Vue.component("g-tabs-pane",TabsPane)
 Vue.use(plugin)
 Vue.component("g-popover",Popver)
+Vue.component("g-collapse",Collapse)
+Vue.component("g-collapse-item",CollapseItem)
 
 
 
